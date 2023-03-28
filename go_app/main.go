c3b291d298d98f8c968f507f7827a57c7bc31681
janet-do <124389477+janet-do@users.noreply.github.com>
2023-03-28 22:26:00 +0000

    added go

---
+++ go_app/main.go
@@ -0,0 +1,27 @@
+// Created by: Janet
+// Created on: Sep 2020
+//
+// This program finds the volume of a cylinder 
+
+package main
+
+import (
+	"fmt"
+	"math"
+)
+
+func main() {
+	// input
+	var radius float64
+	var height float64
+	fmt.Print("Enter the radius of the cylinder (in cm): ")
+	fmt.Scanln(&radius)
+	fmt.Print("Enter the height of the cylinder (in cm): ")
+	fmt.Scanln(&height)
+
+	// process
+	volume := math.Pi * math.Pow(radius, 2) * height
+
+	// output
+	fmt.Printf("The volume of the cylinder is: %.2f cm³\n", volume)
+}
